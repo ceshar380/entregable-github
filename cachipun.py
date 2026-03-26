8494ce8b0a5eb5d6bc7528e571d8de5864aec2a7
ceshar380 <cesartroncoso@estudiante.uc.cl>
2026-03-26 20:20:31 -0300

    tijera epica

--- cachipun.py
+++ cachipun.py
@@ -9,3 +9,23 @@ if jugador.lower() == "rock":
         print("perdiste")
     if resp_compu == "rock":
         print("empate")
+
+
+
+if jugador.lower()== "paper":
+    if resp_compu == "spock" or resp_compu == "rock":
+        print("ganaste")
+    if resp_compu == "scissors" or resp_compu == "lizard":
+        print("perdiste")
+    if resp_compu == "paper":
+        print("empate")
+
+if jugador.lower() == "scissors":
+    if resp_compu == "paper" or resp_compu == "lizard":
+        print("ganaste")
+    if resp_compu == "rock" or resp_compu == "spock":
+        print("perdiste")
+    if resp_compu == "scissors":
+        print("empate")
+
+if jugador.lower()
