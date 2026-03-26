f4b42f0b529a48b21312d9833454161c11cd23b2
ceshar380 <cesartroncoso@estudiante.uc.cl>
2026-03-26 18:27:29 -0300

    logica roca

--- cachipun.py
+++ cachipun.py
@@ -1,3 +1,11 @@
 from random import randint
 jugador = input()
 compu = ["Rock","Paper","Scissors","Lizard", "Spock"]
+resp_compu = compu[randint(0,5)]
+if jugador.lower() == "rock":
+    if resp_compu == "scissors" or resp_compu == "lizard":
+        print("ganaste")
+    if resp_compu == "paper" or resp_compu == "spock":
+        print("perdiste")
+    if resp_compu == "rock":
+        print("empate")
